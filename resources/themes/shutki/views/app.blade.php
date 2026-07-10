bb0adbf55608d5ddcc83c0663f0d5ec3298ab29e
Sozib Alahi <sozibalahi@gmail.com>
2026-07-10 16:47:06 +0600

    fix: add favicon support to theme app.blade.php layout

--- resources/themes/shutki/views/app.blade.php
+++ resources/themes/shutki/views/app.blade.php
@@ -17,6 +17,24 @@
 
         <title inertia>{{ config('app.name', 'শুকটি বাজার') }}</title>
 
+        @if($favicon = \App\Models\SiteSetting::getValue('site_favicon'))
+            @php
+                $faviconUrl = str_starts_with($favicon, 'http') ? $favicon : '/storage/' . $favicon;
+                $extension = pathinfo($favicon, PATHINFO_EXTENSION);
+                $mimeType = match($extension) {
+                    'png' => 'image/png',
+                    'gif' => 'image/gif',
+                    'ico' => 'image/x-icon',
+                    'svg' => 'image/svg+xml',
+                    default => 'image/png'
+                };
+            @endphp
+            <link rel="icon" type="{{ $mimeType }}" href="{{ $faviconUrl }}?v=2">
+            <link rel="shortcut icon" type="{{ $mimeType }}" href="{{ $faviconUrl }}?v=2">
+        @else
+            <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2">
+        @endif
+
         {{-- Google Fonts: Hind Siliguri for Bengali, Lora for headings --}}
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
